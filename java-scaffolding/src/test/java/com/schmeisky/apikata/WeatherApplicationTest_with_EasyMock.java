91d483577ae53489a066c1c9b8a23376ee7f9762
Katharina Laube <blueflower@gmx.de>
2015-05-15 22:34:25 +0200

    Finished test with PowerMock and EasyMock

--- java-scaffolding/src/test/java/com/schmeisky/apikata/WeatherApplicationTest_with_EasyMock.java
+++ java-scaffolding/src/test/java/com/schmeisky/apikata/WeatherApplicationTest_with_EasyMock.java
@@ -1,33 +1,55 @@
 package com.schmeisky.apikata;
 
+import static org.easymock.EasyMock.expect;
+import static org.powermock.api.easymock.PowerMock.replayAll;
+import static org.powermock.api.easymock.PowerMock.verifyAll;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.annotation.Mock;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.junit.Assert.assertEquals;
-
+/**
+ * Test example with PowerMock 1.6.2, EasyMock and JUnit 4.12
+ */
+@RunWith(PowerMockRunner.class)
 public class WeatherApplicationTest_with_EasyMock {
-    private WeatherApplication underTest;
-/* TODO
+    
+	private WeatherApplication underTest;
+
     @Mock private APIAccess apiAccess;
     @Mock private FileOutputPort outputPort;
-    @Mock private Weather weather;
+    
+    private Weather weather;
 
+    private Map<String, Object> testData = new HashMap<String, Object>() {{
+        put("id", 1); 
+        put("name", "Berlin"); 
+        put("time", "2015-04-27 23:00:00"); 
+        put("T", 17.0); 
+        put("P", 1000); 
+        put("D", "N");
+    }};
 
     @Before
     public void setUp() {
-        apiAccess = new APIAccess();
         underTest = new WeatherApplication(apiAccess, outputPort);
+        weather = new Weather(testData);
     }
 
-    public void testLink(){
+    @Test
+    public void testConvertWeather(){
 
         expect(apiAccess.readWeather()).andReturn(weather);
-        outputPort.writeFile();
+        outputPort.writeWeatherString("1,Berlin,2015-04-27,23:00:00,17.0,1000,N");
         replayAll();
 
         underTest.convertWeather();
 
         verifyAll();
     }
-    */
 }
